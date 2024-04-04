211abbd4e0854f71ea7d1069dbf8b00cf760b02d
Kamil Miszel <kamil.miszel@nordcloud.com>
2024-04-04 10:15:18 +0200

    initial commit

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-githubtfstates"
-    storage_account_name = "miszelsandbox"
+    storage_account_name = "miszelidentity"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
